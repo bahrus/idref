a36bd46fffe368763eef8379eed4f01011b6d7c0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-29 08:20:31 -0500

    Initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -15,24 +15,32 @@ class HtmlForAttributeDefinitionProvider implements vscode.DefinitionProvider {
         position: vscode.Position,
         token: vscode.CancellationToken
     ): vscode.ProviderResult<vscode.Definition> {
+        const config = vscode.workspace.getConfiguration('idref');
+        const customAttributes: string[] = config.get('attributes', []);
+        const allAttributes = ['em-bower', 'for', ...customAttributes];
         const line = document.lineAt(position.line).text;
         const wordRange = document.getWordRangeAtPosition(position);
         
         if (!wordRange) return null;
 
-        // Check if we're in a 'for' attribute
-        const forAttributePattern = /for\s*=\s*["']?(\w+)["']?/g;
-        let match;
-        
-        while ((match = forAttributePattern.exec(line)) !== null) {
-            const matchStart = match.index + match[0].indexOf(match[1]);
-            const matchEnd = matchStart + match[1].length;
+        for(const attr of allAttributes){
+            // Check if we're in a 'for' attribute
+            //const regex = /for\s*=\s*["']?(\w+)["']?/g;
+            //const regex = new RegExp(`${attr}\\s*=\\s*["']([^"']+)["']`, 'gi');
+            const regex = new RegExp(`${attr}\\s*=\\s*["']?(\\w+)["']?`, 'gi');
+            let match;
             
-            if (position.character >= matchStart && position.character <= matchEnd) {
-                const targetId = match[1];
-                return this.findElementById(document, targetId);
+            while ((match = regex.exec(line)) !== null) {
+                const matchStart = match.index + match[0].indexOf(match[1]);
+                const matchEnd = matchStart + match[1].length;
+                
+                if (position.character >= matchStart && position.character <= matchEnd) {
+                    const targetId = match[1];
+                    return this.findElementById(document, targetId);
+                }
             }
         }
+
 
         return null;
     }
